e1f955a0acbeee2e928ba0ff1e00fc201b660b9b
jmccarthy14 <jmccarthy14@gmail.com>
2015-08-03 00:41:33 -0700

    fix migrations to have fk restraints

--- migrations/20150803015638-add-project-tasks.js
+++ migrations/20150803015638-add-project-tasks.js
@@ -3,8 +3,36 @@ var type = dbm.dataType;
 
 exports.up = function(db, callback) {
 	db.createTable('project_tasks', {
-		project_id: { type: 'int', primaryKey: true },
-		task_id: { type: 'int', primaryKey: true }
+		project_id: { 
+			type: 'int', 
+			primaryKey: true,
+			foreignKey: {
+				name: 'project_tasks_projects_project_id_fk',
+		        table: 'projects',
+		        rules: {
+					onDelete: 'CASCADE',
+		            onUpdate: 'RESTRICT'
+		        },
+		        mapping: {
+					project_id: 'id'
+				}
+			}
+		},
+		task_id: { 
+			type: 'int', 
+			primaryKey: true,
+			foreignKey: {
+				name: 'project_tasks_tasks_task_id_fk',
+		        table: 'tasks',
+		        rules: {
+					onDelete: 'CASCADE',
+		            onUpdate: 'RESTRICT'
+		        },
+		        mapping: {
+					task_id: 'id'
+				}
+			}
+		}
 	}, callback);
 };
 
--- migrations/20150803020318-add-org-users.js
+++ migrations/20150803020318-add-org-users.js
@@ -3,10 +3,37 @@ var type = dbm.dataType;
 
 exports.up = function(db, callback) {
 	db.createTable('org_users', {
-		org_id: { type: 'int', primaryKey: true },
-		user_id: { type: 'int', primaryKey: true }
+		org_id: { 
+			type: 'int', 
+			primaryKey: true,
+			foreignKey: {
+				name: 'organizations_org_id_fk',
+		        table: 'orgs',
+		        rules: {
+					onDelete: 'CASCADE',
+		            onUpdate: 'RESTRICT'
+		        },
+		        mapping: {
+					org_id: 'id'
+				}
+			}	
+		},
+		user_id: { 
+			type: 'int', 
+			primaryKey: true,
+			foreignKey: {
+				name: 'organizations_user_id_fk',
+		        table: 'users',
+		        rules: {
+					onDelete: 'CASCADE',
+		            onUpdate: 'RESTRICT'
+		        },
+		        mapping: {
+					user_id: 'id'
+				}
+			}	
+		}
 	}, callback);
-
 };
 
 exports.down = function(db, callback) {
