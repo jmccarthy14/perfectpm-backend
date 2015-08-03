09261715a1df14d5ebbef825676e4a6b2a61cf97
jmccarthy14 <jmccarthy14@gmail.com>
2015-08-03 02:24:07 -0700

    cleanup of user endpoint

--- business/views/users-view.js
+++ business/views/users-view.js
@@ -3,21 +3,13 @@ var userService = require('../service/user-service');
 function createUser(req, res) {
 	// Minimal validation here, on api layer.
 	var user = req.body;
-	if(user.firstName && user.lastName && user.email) {
-		userService.createUser(user, function(err, result) {
-			if(err) {
-				if(err.code === 'ER_DUP_ENTRY') {
-					res.status(400).send('A user with this email already exists');
-				} else { 
-					res.status(400).send('Error creating user: ' + err.message);
-				}
-			} else {
-				res.send('Created user: ' + JSON.stringify(result));
-			}
-		});
-	} else {
-		res.status(400).send('Validation error, be sure to include firstName, lastName and email');
-	}
+	userService.createUser(user, function(err, result) {
+		if(err) {
+			res.status(400).send('Error creating user: ' + err.message);
+		} else {
+			res.send('Created user: ' + JSON.stringify(result));
+		}
+	});
 }
 
 module.exports = {
